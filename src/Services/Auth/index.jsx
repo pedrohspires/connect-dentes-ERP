83ad9143877f944a11454136dd1440f25c0fc6e1
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-01-31 15:35:07 -0300

    Services: autenticação

--- src/Services/Auth/index.jsx
+++ src/Services/Auth/index.jsx
@@ -23,3 +23,27 @@ export const Login = async (dadosRequest) => {
     }
   }
 }
+
+export const Register = async (dadosRequest) => {
+  const axios = await instance();
+
+  try{
+    const response = await axios.post('/Usuario/Cadastro', dadosRequest);
+    return {
+      sucesso: true,
+      data: response.data
+    }
+  } catch(error){
+    if(!error.message){
+      return {
+        sucesso: false,
+        mensagem: "Não foi possível conectar ao servidor!"
+      }
+    }
+
+    return {
+      sucesso: false,
+      mensagem: error.response.data
+    }
+  }
+}
