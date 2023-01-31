5d1e769eb6ff1c106f39154e311d08515f290d1a
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-01-31 15:35:59 -0300

    Correção: as cores da paleta personalizada substituiam as originais

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,16 +5,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      paleta: {
-        100: "#cbdad5",
-        300: "#89a7b1",
-        500: "#566981",
-        700: "#3a415a",
-        900: "#34344e"
+    extend: {
+      colors: {
+        paleta: {
+          100: "#cbdad5",
+          300: "#89a7b1",
+          500: "#566981",
+          700: "#3a415a",
+          900: "#34344e"
+        }
       }
-    }
+    },
   },
   plugins: [],
 }
