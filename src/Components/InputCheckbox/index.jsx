b7ab0974d43695812dd46b420cd5d24f7ac95408
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-02-10 14:46:33 -0300

    Checkbox com radix-ui personalizado

--- src/Components/InputCheckbox/index.jsx
+++ src/Components/InputCheckbox/index.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import React from 'react';
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { BsCheckLg } from 'react-icons/bs';
 
-function InputCheckbox({ label, checked, onChange }) {
-  return (
-    <div>
-      <input 
-        className='w-8' 
-        type="checkbox" 
-        name={label} 
-        id={label} 
-        checked={checked}
-        onChange={(event) => {
-          onChange(event.target.checked, label)
-        }}
-      />
-      <label htmlFor={label}>{label}</label>
-    </div>
-  )
-}
+const InputCheckbox = ({ id, label, checked, onChange, customClassName }) => (
+  <div className={customClassName || 'flex items-center'}>
+    <Checkbox.Root 
+      className='bg-paleta-500 w-6 h-6 m-0.5 rounded-md flex items-center justify-center shadow-lg
+                  hover:bg-paleta-700 focus:shadow-none' 
+      id={id || label}
+      checked={checked}
+      onCheckedChange={() => onChange(!checked, label)}
+    >
+      <Checkbox.Indicator className='text-white'>
+        <BsCheckLg size={12}/>
+      </Checkbox.Indicator>
+    </Checkbox.Root>
+    
+    <label className='color-paleta-900 text-base pl-1' htmlFor={id || label}>
+      {label}
+    </label>
+  </div>
+);
 
-export default InputCheckbox
+export default InputCheckbox;
